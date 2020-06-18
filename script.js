0a41508608edddca66489baa02af23c32e16a890
leonardodistefano <37075539+leonardodistefano@users.noreply.github.com>
2020-06-18 17:55:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function main() {
   const scene = new THREE.Scene();
 
   {
-    const skyColor = B1E1FF;  // light blue
+    const skyColor = 0xB1E1FF;  // light blue
     const groundColor = 0xB97A20;  // brownish orange
     const intensity = 2;
     const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
